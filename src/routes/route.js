55104c9feffd89f5ca9b3866a79ba256f0f5fc13
jiyalal <106508333+jiyalal@users.noreply.github.com>
2022-06-03 00:26:07 +0530

    assignment/node.js-module problem-4

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,8 @@
-const express = require('express')
+  const express = require('express')
   const tyagi1 = require('../logger/logger')
   const tyagi2 = require('../util/helper')
   const tyagi3 = require('../validator/formatter')
+  const lodash = require('lodash')
     
 
 
@@ -20,24 +21,38 @@ router.get('/test-me', function (req, res) {
 
         
     
-    res.send('My first ever api!')
+    res.send('this is jk tygi api!')
 });
 
-router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
+router.get('/hello', function (req, res) {
+    let nameMonth = ["Jan","Feb","March","April","May","Jun","July","August","Sep","Oct","Nov","December"]
+    const chunk = lodash.chunk(nameMonth,3);
+    console.log(chunk);
+
+    const oddNo = [1,3,5,7,9,11,13,15,17,19]
+     const tail = lodash.tail(oddNo);
+     console.log(tail);
+
+     const arr1 = [1,3,5,7]
+     const arr2 = [1,3,8,7]
+     const arr3 = [1,3,9,7]
+     const arr4 = [1,10,5,7]
+     const arr5 = [1,3,5,11]
+     const mixarr = lodash.union(arr1,arr2,arr3,arr4,arr5)
+     console.log(mixarr);
+
+     const a = [["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","pans Labyrinth"]]
+     const b = lodash.fromPairs(a)
+     console.log(b);
+
+     
+    res.send('jk tyagi 2nd API!')
 });
 
 router.get('/test-me2', function (req, res) {
     res.send('My third api!')
 });
 
-router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
-});
-
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
 
 module.exports = router;
 // adding this comment for no reason
