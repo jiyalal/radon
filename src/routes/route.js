291f92820281c457d0475a94a40e4d8c692fb338
jiyalal <106508333+jiyalal@users.noreply.github.com>
2022-06-04 22:45:33 +0530

    assignment/get-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,41 +1,47 @@
 const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
+const tyagi = require('../routes/route')
+const _   =  require('underscore')
+
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    myHelper.printDate()
-    myHelper.getCurrentMonth()
-    myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-    console.log('The first element received from underscope function is '+firstElement)
-    res.send('My first ever api!')
+let array = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins'];
+
+router.get('/movies', function (req, res) {
+    res.send(array);
 });
 
-router.get('/hello', function (req, res) {
-   
-    res.send('Hello there!')
-});
-
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
+router.get("/movies/:index", (req, res) => {
+  let index = req.params['index'];
+  
+  if(index>3){
+    res.send('ERROR! Please Provide a Valid Index Number...');
+  }
+  else{
+    res.send(array[index]);
+  }
 })
 
-router.get('/candidates/:canidatesName', function(req, res){
-    console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
-    res.send('Done')
+router.get('/films', (req, res) => {
+  result = _.map(array, function(value, index){
+    return {"id": index+1, "name": value};
+ });
+ res.send(result);
 })
 
+router.get('/films/:filmId', (req, res) => {
+  let filmId = req.params['filmId'];
+  
+  if(filmId>4){
+    res.send('ERROR! No movie exists with this id');
+  }
+  else{
+    res.send({
+      "id": filmId,
+      "name": array[filmId-1]
+    });
+  }
+})
 
 module.exports = router;
 // adding this comment for no reason
