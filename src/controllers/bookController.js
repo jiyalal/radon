61354835d151e60fa5c36aeb2e5c5cabfa6420b9
jiyalal <106508333+jiyalal@users.noreply.github.com>
2022-06-07 16:49:02 +0530

    session/mongo-session2-part1

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,4 +1,5 @@
-const BookModel= require("../models/bookModel")
+const bookModel = require("../models/bookModel")
+
 
 const createBook= async function (req, res) {
     let data= req.body
@@ -7,10 +8,49 @@ const createBook= async function (req, res) {
     res.send({msg: savedData})
 }
 
-const getBooksData= async function (req, res) {
-    let allBooks= await BookModel.find(  { authorName : "SK" , isPublished: true }  )
-    res.send({msg: allBooks})
+const bookList= async function (req, res) {
+    let data= req.body
+
+    let savedData= await BookModel.find().select({bookName: 1, authorName: 1, _id:0})
+    res.send({msg: savedData})
 }
 
-module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
+const getBooksInYear = async (req, res) => {
+    let data = req.query.year
+    
+    let book = await  BookModel.find({year:{$eq:data}}).select({bookName:1, _id:0})
+    res.send({msg: book})
+}
+
+const getParticularBooks = async (req, res) =>{
+    let condition = req.query
+    let Books = await bookModel.find(condition).select({bookName:1,_id:0})
+    // console.log(condition)
+    res.send({msg: Books})
+    // console.log(Books)
+}
+
+const getXINRBooks = async (req, res) => {
+    let book = await BookModel.find( {"prices.indianPrice": [100, 200, 500]})//{ "prices.indianPrice": { $in: ["100INR", "200INR", "500INR"] } }
+
+    res.send({msg: book})
+}
+
+const getRandomBooks = async (req, res) => {
+    let books = await BookModel.find({$or: [{"prices.indianPrice": "500INR"}, {$gte:{"totalPages": 500}}]})
+
+    res.send({msg: books})
+}
+
+
+module.exports.createBook=createBook
+
+module.exports.bookList=bookList
+
+module.exports.getBooksInYear=getBooksInYear
+
+module.exports.getParticularBooks=getParticularBooks
+
+module.exports.getXINRBooks=getXINRBooks
+
+module.exports.getRandomBooks= getRandomBooks
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,17 +1,26 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
+    bookName: {
+        type: String,
+        required: true
+    },
+
     prices: {
         indianPrice: String,
-        europePrice: String,
+        europePrice: String
     },
-    sales: {type: Number, default: 10}
-}, { timestamps: true });
+    year: {
+        type: Number,
+        default: 2021
+    },
+    tages: [String],
+    authorName: String,
+    totalPages: Number,
+    stockAvailable: Boolean,
+}, { timestamps: true});
+
+    
 
 
 module.exports = mongoose.model('Book', bookSchema) //users
