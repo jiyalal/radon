6897bfaecfab1580d426a799e7ee0bba9869bb2b
jiyalal <106508333+jiyalal@users.noreply.github.com>
2022-06-12 16:50:09 +0530

    assignment/session/populate-reference

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://jiyalal:DHJg7rF8UKePVpYi@cluster0.8arzf.mongodb.net/jiyalal?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
